a6e58242a3756fb2a0a2e8a63082fc860af0e4ad
H4ymak3r <mirko.damonte@hotmail.com>
2025-01-14 14:10:52 +0100

    Aggiungere i file di progetto.

---
+++ Somma/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Primo numero: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Secondo numero: ");
+int b = int.Parse(Console.ReadLine());
+
+Somma somma = new Somma();
+
+Console.WriteLine(somma.returnSomma(a, b));
---
+++ SommaTest/TestClasse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SommaTest
+{
+    public class TestClasse
+    {
+        [Fact]
+        public void ReturnSomma_ShouldReturnCorrectSum()
+        {
+            Somma somma = new Somma();
+            int a = 5;
+            int b = 7;
+
+            int result = somma.returnSomma(a, b);
+
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void ReturnSomma_ShouldHandleNegativeNumbers()
+        {
+            Somma somma = new Somma();
+            int a = -3;
+            int b = 7;
+
+            int result = somma.returnSomma(a, b);
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void ReturnSomma_ShouldReturnZeroWhenBothNumbersAreZero()
+        {
+            Somma somma = new Somma();
+            int a = 0;
+            int b = 0;
+
+            int result = somma.returnSomma(a, b);
+
+            Assert.Equal(0, result);
+        }
+    }
+}
